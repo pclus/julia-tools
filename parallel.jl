5f7095ca485b217d9761a4d5e64bf0d92b0e15e5
pclus <pau.clusella@upf.edu>
2023-01-11 13:04:53 +0100

    Changes in parallel

--- parallel.jl
+++ parallel.jl
@@ -14,3 +14,17 @@ a = zeros(10);
 Threads.@threads for i = 1:10
 	a[i] = Threads.threadid()
 end
+
+# Keep track of processes with atomic addition:
+
+
+state = Threads.Atomic{Int}(0);
+a=zeros(1000);
+
+# 
+Threads.@threads for id=1:1000
+    Threads.atomic_add!(state, 1)
+    print("-->",state[]," out of 1000\n");
+    flush(stdout);
+    a[id]=rand();
+end
