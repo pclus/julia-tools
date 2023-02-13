a3d0906a68decce76aefb697bc822f71db5be0c3
pclusella <pau.clusella@upf.edu>
2023-02-13 11:06:06 +0100

    small changes in dsp

--- bandpassfilter.jl
+++ bandpassfilter.jl
@@ -1,6 +1,7 @@
 # From this answer: https://discourse.julialang.org/t/filtfilt-in-dsp-jl-i-really-dont-get-it/10437/2
 
 using DSP
-myfilter = digitalfilter(Bandpass(0.1,0.15),Butterworth(2));
+fs = 1000.0 # kHz (sampling rate) 
+myfilter = digitalfilter(Bandpass(0.1,0.15; fs=2500.0),Butterworth(2));
 mydata = rand(100);
 filtfilt(myfilter,mydata)
